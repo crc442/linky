03e56c02b97a493a06909a2842a192687f1786bd
crc442 <EequalsMCsquare91@gmail.com>
2015-03-03 03:30:30 -0500

    some changes

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,11 +3,13 @@
 // var settings = new Store("settings", {
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
-
+chrome.pageAction.show(sender.tab.id);
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
+  	
     sendResponse();
+
+   console.log(request.content);
   });
